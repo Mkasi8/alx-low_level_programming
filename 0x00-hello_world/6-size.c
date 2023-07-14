c8f5ae328f2ce2d91b931087711870d4947aab1e
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-14 02:54:50 -0700

    C-hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of char : %li byte(s)\n", sizeof(char));
+	printf("Size of a char : %li byte(s)\n", sizeof(char));
 	printf("Size of an int : %li byte(s)\n", sizeof(int));
-	printf("Size of long int : %li byte(s)\n", sizeof(long int));
-	printf("Size of long int : %li byte(s)\n", sizeof(long long int));
-	printf("Size of float : %li byte(s)\n", sizeof(float));
+	printf("Size of a long int : %li byte(s)\n", sizeof(long int));
+	printf("Size of a long int : %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float : %li byte(s)\n", sizeof(float));
 	return (0);
 }
