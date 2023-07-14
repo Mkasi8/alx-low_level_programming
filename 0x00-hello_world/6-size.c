86e090519396f6c8d3dbedbe5907a6f39ad12c02
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-14 02:43:27 -0700

    C-hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char : %li byte(s)", sizeof(char));
+	printf("Size of an int : %li byte(s)", sizeof(int));
+	printf("Size of long int : %li byte(s)", sizeof(long int);
+	printf("Size of long int : %li byte(s)", sizeof(long long int);
+	printf("Size of float : %li byte(s)", sizeof(float);
+	return (0);
+}
