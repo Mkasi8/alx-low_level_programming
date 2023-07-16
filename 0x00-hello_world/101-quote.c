07770d39f325b15c22887d971f26d3ad824a72dc
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-16 10:13:09 -0700

    C-hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 /*
  * main - Starting point
+ * Description: prints a line of string the way it is
  * returns 1
  */
 
@@ -12,6 +13,6 @@ int main(void)
 	l = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", l);
-	
+
 	return (1);
 }
