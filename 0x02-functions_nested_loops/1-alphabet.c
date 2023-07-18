74630d655526a56c305aac9e816857491c1f1e6e
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-18 16:05:25 -0700

    Prints lowercase alphabets using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main -  prints the alphabet, in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char count;
+
+	count = a;
+
+	while (count <= z)
+		_putchar(count);
+
+	_putchar('\n');
+
+	return (0);
+}
