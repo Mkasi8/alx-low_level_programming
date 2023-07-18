7e496835eebbda72dc901d5514c95d7936e44d1c
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-18 16:42:02 -0700

    prints lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * main -  prints the alphabet, in lowercase
- *
- * Return: Always 0
+ * print_alphabet - Entry point
+ * Return:0
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	char count;
+	char c = 'a';
 
-	count = 'a';
-
-	while (count <= 'z')
+	while (c <= 'z')
 	{
-		_putchar(count);
-		count++;
+		_putchar(c);
+		c++;
+
 	}
 	_putchar('\n');
 
-	return (0);
 }
