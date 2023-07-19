b206461b6279b62c7def9bfc64dcd05358b5f8d6
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-18 17:59:11 -0700

    Prints last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print las digit of a number
+ * @n: number to print last digit of
+ * Return: n
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+	
+	last_digit = n % 10;
+	_putchar(last_digit);
+
+	return (last_digit);
+}
+
