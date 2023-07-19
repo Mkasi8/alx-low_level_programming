d94f048833baf6a35bdcae676fa3bb5fabe8fbd9
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-18 18:31:13 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds up two integer numbers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+
+	return (a + b);
+}
