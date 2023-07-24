fa7a9dc8e363d85c654d7e1d94e1ad3742003a2d
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-24 14:07:34 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the characters of a string in reverse
+ *
+ * @s: the string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	int length;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	while (s[i] != s[0])
+	{
+		_putchar(s[length]);
+		length--;
+	}
+
+	_putchar('\n');
+}
+
+
