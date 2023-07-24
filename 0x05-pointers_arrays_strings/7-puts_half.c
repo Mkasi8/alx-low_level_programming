e65f817cc5b27c5f2e26c8a9e791ee2276619cae
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-24 15:24:31 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,13 @@
 
 void puts_half(char *str)
 {
-	int length, i, j;
+	int length, i;
 
 	for (length = 0; str[length] != '\0'; length++)
 	{
 	}
 
 	i = (length - 1) / 2;
-	j = 0;
 
 	while (str[i] != str[length-1])
 	{
