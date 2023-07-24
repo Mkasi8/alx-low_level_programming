9b246fd1a44e3731b953757a3b24f282e6d71e1b
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-24 10:00:54 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ *
+ * @s: the string to return its value
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length;
+
+	for (i = 0, s[i] != '\n', i++)
+		length++;
+	return (length);
+}
