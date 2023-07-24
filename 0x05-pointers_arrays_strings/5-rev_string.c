ab9d2d7a438eaafb32007fcdbed885aeaaebc3f5
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-24 14:46:33 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ *
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int length, j, i;
+
+	for (length = 0, s[length] != '\0', length++)
+	{
+	}
+
+	j = length - 1
+	i = 0;
+	char v1, v2;
+
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+
+		s[i] = v2;
+		s[j] = v1;
+
+		i++;
+		j--;
+	}
+}
