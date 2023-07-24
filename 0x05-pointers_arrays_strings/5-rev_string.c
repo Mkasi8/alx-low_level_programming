5a1615be712acf63bb9dd7a5ccf8d3623a027ec3
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-07-24 14:53:06 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,14 @@ void rev_string(char *s)
 {
 	int length, j, i;
 
+	char v1, v2;
+
 	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
 
 	j = length - 1;
 	i = 0;
-	char v1, v2;
 
 	while (j > i)
 	{
