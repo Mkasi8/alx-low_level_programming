19f2dcf60163d66ed5169c685acbf37a49a41e4f
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-08-04 17:18:27 -0700

    Command Line Arguments

---
+++ 0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: array of of pointers to char
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	preturn (0);
+}
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,15 +6,16 @@
  * Return: n
  */
 
+
 char *string_toupper(char *n)
 {
-	int i = 0;
+	int i;
 
-
+	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= a && n[i] <= z)
-		n[i] -= 32;
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
 		i++;
 	}
 	return (n);
