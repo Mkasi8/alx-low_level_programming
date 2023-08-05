9f277ad9274f4cc195acc494ede471df97b02097
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-08-04 17:48:18 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the arguments passed into it.
+ * @argc: argument count
+ * @argv:argument array
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
