c058d8cb637112468320359cf601822f7e5d9714
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-08-04 17:28:51 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, int *argv[])
+{
+	printf("%d\n", argc - 1)
+	return (0);
+}
