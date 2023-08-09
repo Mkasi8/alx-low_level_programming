d48a6a057441f999608d7bef7a4096731f8d2809
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-08-09 01:40:15 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,15 @@ char *create_array(unsigned int size, char c)
 {
 	char *arr;
 
+	int i;
+
 	if (size == 0)
 		return (NULL);
 
 	arr = malloc(sizeof(*arr)*size);
 	if (arr == NULL)
 		return (NULL);
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		arr[i] = c;
 	return (arr);
 }
