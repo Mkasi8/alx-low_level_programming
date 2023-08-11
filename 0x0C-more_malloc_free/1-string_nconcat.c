dc3f32164b8c47047d0073ed811c2e4625ea2a7a
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-08-11 16:15:57 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int len1;
 
 	for (len = 0; s1[len] != '\0'; len++)
-	for (len1 = 0; s2[len1] != '\0')
+	for (len1 = 0; s2[len1] != '\0'; len1++)
 
 	if (n < len1)
 		ptr = malloc(len + n + sizeof(char) - 1);
