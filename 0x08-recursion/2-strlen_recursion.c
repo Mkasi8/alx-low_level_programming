6169f86825f7592c2249b57070fc1aae43b9be6d
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2023-09-27 09:35:01 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return length of a string
+ * @s: string to calculate the length
+ * return: int(length)
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 1;
+
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return n + _strlen_recursion(n+1);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,3 @@
 0- prints a string followed by a new line
 1-prints string in reverse
+2- returns string length
